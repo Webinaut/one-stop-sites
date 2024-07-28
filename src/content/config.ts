9c7ca9af3d75505f19bf91a53e6d537d53178d2d
Martin <slackgt@bigpond.com>
2024-07-28 21:38:29 +1000

    5.6

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,13 +3,14 @@ import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const portfolioCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    hero: z.string(),
-    ranking: z.number(),
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      hero: image(),
+      ranking: z.number(),
+      tags: z.array(z.string()),
+    }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
