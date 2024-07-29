f1c630a0bf1e81911fa31648b1104ee498bbe950
Martin <slackgt@bigpond.com>
2024-07-29 17:30:50 +1000

    6.2

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,4 @@
-// Toggle Hamburger
+// Toggle Hamburger Menu
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".main-nav").classList.toggle("expanded");
   document.querySelector(".hamburger").classList.toggle("expanded");
