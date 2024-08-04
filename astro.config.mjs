a0219f5f8cbb40196455114975671a3223e1cf2e
Martin <slackgt@bigpond.com>
2024-08-04 15:34:26 +1000

    10.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "always",
-  integrations: [sitemap()]
+  site: "https://onestopsites.net.au",
+  integrations: [sitemap()],
 });
